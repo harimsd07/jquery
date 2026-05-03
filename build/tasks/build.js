155066485de366b3edc38b732defb77b77771d5b
Irah <119393196+harimsd07@users.noreply.github.com>
2026-05-03 17:55:56 +0530

    LegacyFix: modernize build/tasks/build.js

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -218,7 +218,7 @@ export async function build( {
 		setOverride(
 			"./src/exports/global.js",
 			"import { jQuery } from \"../core.js\";\n\n" +
-				"jQuery.noConflict = function() {};"
+				"jQuery.noConflict = () => {};"
 		);
 		excluded.splice( index, 1 );
 	}
